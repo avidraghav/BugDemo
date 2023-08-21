5b53f5b5f50b08e6498aba12bbb5fa235989437f
avidraghav <raghavaggarwal776@gmail.com>
2023-08-21 16:24:26 +0530

    changes

--- app/src/main/java/com/example/bugdemo/MainActivity.kt
+++ app/src/main/java/com/example/bugdemo/MainActivity.kt
@@ -16,10 +16,8 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
@@ -36,7 +34,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
                 ) {
                     DemoLayout()
                 }
@@ -47,16 +45,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoLayout() {
-    var guess by remember { mutableStateOf("") }
+    val guess = remember { mutableStateOf("") }
 
     Column(verticalArrangement = Arrangement.spacedBy(24.dp)) {
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
+            horizontalArrangement = Arrangement.SpaceAround,
         ) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
-                    text = stringResource(id = R.string.image_composable)
+                    text = stringResource(id = R.string.image_composable),
                 )
 
                 /*
@@ -67,12 +65,12 @@ fun DemoLayout() {
                     modifier = Modifier
                         .size(80.dp),
                     painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = null
+                    contentDescription = null,
                 )
             }
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
-                    text = stringResource(id = R.string.box_composable)
+                    text = stringResource(id = R.string.box_composable),
                 )
                 /*
                  * Box doesn't recomposes whenever OutlinedTextField recomposes as they don't
@@ -82,18 +80,20 @@ fun DemoLayout() {
                     modifier = Modifier
                         .size(80.dp)
                         .paint(
-                            painterResource(id = R.drawable.ic_launcher_background)
-                        )
+                            painterResource(id = R.drawable.ic_launcher_background),
+                        ),
                 )
             }
         }
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
-            value = guess,
-            onValueChange = { guess = it },
+            value = guess.value,
+            onValueChange = {
+                guess.value = it
+            },
             label = {
                 Text(text = stringResource(R.string.enter_any_text))
-            }
+            },
         )
     }
 }
